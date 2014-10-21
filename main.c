fb58fef50e7fc7c5dbe4b7a1aeec8b750347d0b1
vary75 <vary75@naver.com>
2014-10-21 17:32:50 +0900

    main.c 파일 수정
    내용 수정

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+17:32분 수정
+:수정 내용
+
+
 pull request test
 
 2014.10.21
@@ -6,15 +10,3 @@ test code input.
 hihihihihihihihCiCCi
 hello world
 ㅇㅇㅇ
-
-
-
-
-LG폰인지? 네이블 폰인지?
-- 아이폰3G(07043790564), 88948B1P3NR
-- 아이폰5  C2YJRGYTDTWF
-
-
-둘다 UMC
-- 88948B1P3NR
-- C2YJRGYTDTWF
